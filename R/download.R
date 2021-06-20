32737638d16232b58d1ba7539bd7474acd255e2e
GuilhermeFCO <53825689+GuilhermeFCO@users.noreply.github.com>
2021-06-20 16:21:15 -0300

    retirando Rproj

--- R/download.R
+++ R/download.R
@@ -1,3 +1,12 @@
 downloadCovidBR <- function(dir = "./") {
 
+	brazil <- covid19br::downloadCovid19(level = "brazil")
+	regions <- covid19br::downloadCovid19(level = "regions")
+	states <- covid19br::downloadCovid19(level = "states")
+	cities <- covid19br::downloadCovid19(level = "cities") %>% dplyr::filter(city != "")
+
+	indexes <- data.table::fread("https://raw.githubusercontent.com/GuilhermeFCO/dataBaseCovid19/main/indices.csv")
+
+
+
 }
